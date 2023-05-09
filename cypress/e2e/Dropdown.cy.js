50d0362ebd16f4538c321cf9f29270fd2c2c8513
Anilkumarotte <113018521+Anilkumarotte@users.noreply.github.com>
2023-05-09 19:17:28 +0530

    Update Dropdown.cy.js

--- cypress/e2e/Dropdown.cy.js
+++ cypress/e2e/Dropdown.cy.js
@@ -5,5 +5,25 @@ describe('Tutorialspoint', function () {
        cy.visit("https://register.rediff.com/register/register.php")
        //select option India with value then verify with assertion
        cy.get('select[id="country"]').select('12').should('have.value', '12')
+        
+       ///Get dropdown text
+        
+           Dropdown.then(dropdown => {
+        const dropdownoptions = dropdown.find('option');
+        dropdownoptions.each((index, option) => {
+          const dropdownoptionText = Cypress.$(option).text();
+         cy.log(dropdownoptionText);
+        
+         });
+        });
+        ///get count 
+        cy.get('.form-select') .invoke('children').its('length').then((count) => {              
+            cy.log(`Number of items in the dropdown: ${count}`)
+          });
+        
     })
+    
+    
  })
+
+

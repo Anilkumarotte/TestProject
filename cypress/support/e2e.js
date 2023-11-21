988432f92850ced08c263df929d02a58af621cd7
Anilkumarotte <113018521+Anilkumarotte@users.noreply.github.com>
2023-11-21 19:38:02 +0530

    Update e2e.js

--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -19,3 +19,4 @@ require('@cypress/xpath');
 import 'cypress-iframe'
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+const app = window.top; if (!app.document.head.querySelector("[data-hide-command-log-request]")) { const style = app.document.createElement("style"); style.innerHTML=".command-name-request, .command-name-xhr { display: none}"; style.setAttribute("data-hide-command-log-request", ""); app.document.head.appendChild(style); }
